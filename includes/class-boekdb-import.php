b743f94a8bf6e3010addd3de42dbcb3930117f30
omeblues <omeblues@gmail.com>
2021-05-07 14:38:42 +0200

    basic metadata in admin

--- includes/class-boekdb-import.php
+++ includes/class-boekdb-import.php
@@ -20,15 +20,15 @@ class BoekDB_Import
      */
     public static function init()
     {
-        if ( ! wp_next_scheduled( self::CRON_HOOK ) ) {
-            wp_schedule_event( time(), 'daily', self::CRON_HOOK);
+        if (!wp_next_scheduled(self::CRON_HOOK)) {
+            wp_schedule_event(time(), 'daily', self::CRON_HOOK);
         }
         add_action(self::CRON_HOOK, array(self::class, 'import'));
     }
 
     public static function import()
     {
-        $curl = curl_init('http://boekdb.v2.test/api/json/v1/products?updated_at=2020-01-26T11%3A49%3A37%2B01%3A00');
+        $curl          = curl_init('http://boekdb.v2.test/api/json/v1/products?updated_at=2020-01-26T11%3A49%3A37%2B01%3A00');
         $authorization = "Authorization: Bearer j8mG6QORW04kgiEwH3G7hybmm0gEKU32dNUmyVtFGC08YXt9sRHlzkH8WTGkp7IJ";
 
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
@@ -43,10 +43,32 @@ class BoekDB_Import
         curl_close($curl);
         $products = json_decode($result);
         echo '<pre>';
-        foreach($products as $product) {
-            $boek = array();
-            $boek['titel'] = $product->titel;
-            $boek['isbn'] = $product->isbn;
+        foreach ($products as $product) {
+            $boek                        = array();
+            $boek['titel']               = $product->titel;
+            $boek['isbn']                = $product->isbn;
+            $boek['subtitel']            = $product->subtitel;
+            $boek['deeltitel']           = $product->deeltitel;
+            $boek['sectietitel']         = $product->sectietitel;
+            $boek['origineletitel']      = $product->origineletitel;
+            $boek['serietitel']          = $product->serietitel;
+            $boek['deel']                = $product->deel;
+            $boek['druk']                = $product->druk;
+            $boek['uitgever']            = $product->uitgever;
+            $boek['imprint']             = $product->imprint;
+            $boek['flaptekst']           = $product->flaptekst;
+            $boek['annotatie']           = $product->annotatie;
+            $boek['taal']                = $product->taal;
+            $boek['illustraties']        = $product->illustraties;
+            $boek['leeftijdscategorie']  = $product->leeftijdscategorie;
+            $boek['avi']                 = $product->avi;
+            $boek['eersteuitleverdatum'] = $product->eersteuitleverdatum;
+            $boek['verschijningsdatum']  = $product->verschijningsdatum;
+            $boek['prijs']               = $product->prijs;
+            $boek['actieprijs']          = $product->actieprijs;
+            $boek['actieperiode_start']  = $product->actieperiode_start;
+            $boek['actieperiode_einde']  = $product->actieperiode_einde;
+
 //            $boek['subtitle'] = (string)$book_element->SubTitel;
 //            $boek['nur'] = (string)$book_element->Uitgave->Nurcode;
 //            $boek['genre'] = (string)$book_element->Uitgave->Genre->Omschrijving;
@@ -66,23 +88,23 @@ class BoekDB_Import
 
 
             $post_id = null;
-            $post = array(
-                'ID'            => $post_id,
-                'post_status'   => 'publish',
-                'post_type'     => 'boekdb_boek',
-                'post_title'    => $boek['titel'],
-                'post_name'     => sanitize_title($boek['titel'])
+            $post    = array(
+                'ID'          => $post_id,
+                'post_status' => 'publish',
+                'post_type'   => 'boekdb_boek',
+                'post_title'  => $boek['titel'],
+                'post_name'   => sanitize_title($boek['titel'])
             );
 
             // create/update post
-            if(is_null($post_id)) {
+            if (is_null($post_id)) {
                 $post_id = wp_insert_post($post);
             } else {
                 $post_id = wp_update_post($post);
             }
 
-            foreach($boek as $key => $value) {
-                switch($key) {
+            foreach ($boek as $key => $value) {
+                switch ($key) {
                     default:
                         update_post_meta($post_id, $key, $value);
                         break;
--- includes/class-boekdb-post-types.php
+++ includes/class-boekdb-post-types.php
@@ -21,6 +21,7 @@ class Boekdb_Post_Types
     {
         add_action('init', array(__CLASS__, 'register_taxonomies'), 5);
         add_action('init', array(__CLASS__, 'register_post_types'), 5);
+        add_action('add_meta_boxes', array(__CLASS__, 'add_metaboxes'), 5);
     }
 
     /**
@@ -51,22 +52,22 @@ class Boekdb_Post_Types
     protected static function register_medewerker_taxonomy()
     {
         $labels = array(
-            'name'                       => 'Medewerkers Taxonomie',
-            'singular_name'              => 'Medewerker Taxonomie',
+            'name'          => 'Medewerkers Taxonomie',
+            'singular_name' => 'Medewerker Taxonomie',
         );
-        $args = array(
-            'labels'                     => $labels,
-            'hierarchical'               => false,
-            'public'                     => false,
-            'rewrite'                    => false,
-            'query_var'                  => false,
-            'show_ui'                    => true,
-            'show_admin_column'          => true,
-            'show_in_nav_menus'          => true,
-            'show_tagcloud'              => true,
+        $args   = array(
+            'labels'            => $labels,
+            'hierarchical'      => false,
+            'public'            => false,
+            'rewrite'           => false,
+            'query_var'         => false,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'show_in_nav_menus' => true,
+            'show_tagcloud'     => true,
         );
 
-        register_taxonomy( 'boekdb_medewerker_tax', array( 'boekdb_boek', 'boekdb_medewerker' ), $args );
+        register_taxonomy('boekdb_medewerker_tax', array('boekdb_boek', 'boekdb_medewerker'), $args);
     }
 
     protected static function register_medewerker_post_type()
@@ -82,25 +83,23 @@ class Boekdb_Post_Types
 
         register_post_type(
             'boekdb_medewerker', array(
-                'labels'       => $labels,
-                'has_archive'  => true,
-                'public'       => true,
-                'hierarchical' => false,
-                'supports'     => array(
+                'labels'          => $labels,
+                'has_archive'     => true,
+                'public'          => true,
+                'hierarchical'    => false,
+                'supports'        => array(
                     'title',
-                    'editor',
-                    'excerpt',
                     'custom-fields',
                     'thumbnail',
-                    'page-attributes'
                 ),
-                'capabilities' => array(
+                'capability_type' => 'page',
+                'capabilities'    => array(
                     'create_posts' => 'do_not_allow',
                 ),
-                'map_meta_cap' => true,
+                'map_meta_cap'    => true,
                 //'taxonomies'   => 'category',
-                'rewrite'      => array('slug' => 'medewerker'),
-                'show_in_rest' => true
+                'rewrite'         => array('slug' => 'medewerker'),
+                'show_in_rest'    => true
             )
         );
     }
@@ -124,11 +123,7 @@ class Boekdb_Post_Types
                 'hierarchical' => false,
                 'supports'     => array(
                     'title',
-                    'editor',
-                    'excerpt',
-                    'custom-fields',
                     'thumbnail',
-                    'page-attributes'
                 ),
                 'capabilities' => array(
                     'create_posts' => 'do_not_allow',
@@ -139,6 +134,28 @@ class Boekdb_Post_Types
                 'show_in_rest' => true
             )
         );
+    }
+
+    public static function add_metaboxes()
+    {
+        add_meta_box(
+            'boek_fields',
+            'Velden', // Title of metabox
+            array(__CLASS__, 'metabox_html'), // Function that prints out the HTML for metabox
+            'boekdb_boek',
+            'normal',
+            'high'
+        );
+    }
+
+    public static function metabox_html( $boek )
+    {
+        $meta = get_post_meta($boek->ID);
+        echo '<table>';
+        foreach($meta as $name => $value) {
+            echo '<tr><th style="vertical-align: top; text-align: left;">'.$name.'</th><td>'.$value[0].'</td></tr>';
+        }
+        echo '</table>';
     }
 }
 
