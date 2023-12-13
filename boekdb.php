6fa061484a15cf98ca0144786ec91686b5e4b4b6
kevin <kevin@icontact.nl>
2023-12-13 15:04:38 +0100

    refactor(boekdb): Remove debug statement from boekdb_modify_boek_permalink function

--- boekdb.php
+++ boekdb.php
@@ -330,8 +330,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 	 */
 	function boekdb_modify_boek_permalink($post_link, $post) {
 		if ('boekdb_boek' === $post->post_type) {
-			boekdb_debug('Modifying permalink for post ' . $post->ID);
-
 			// Try to get the link from the cache
 			$post_link = get_transient('boekdb_permalink_' . $post->ID);
 
